3dd6cbbfee19fc2d93fc59a0b77f5775ebb12f66
Daniel Héctor Gutierrez <dgutierrez@minerar.com.ar>
2026-05-17 21:06:56 -0300

    Update malagueno-vcp.js

--- api/malagueno-vcp.js
+++ api/malagueno-vcp.js
@@ -1,15 +1,17 @@
 export default async function handler(req, res) {
-  const CLAVE_SECRETA = process.env.MI_CLAVE_SECRETA;
-  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx98sAzjJFQdWmikw1hCsZVp1M79lKxcGJEyb-IkgkQPj3P5JnEjN9ypG6jfOVE19tBuA/exec';
+  const CLAVE_SECRETA = 'PMMasAgiles1122SEG';
+  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx0jGx6mdQJyvYa11gerDHpqirDkAxiflTd-nNy01SQ0vyP2pSZzGoBCBL_tPGMoZ8/exec';
 
   try {
-    const respuesta = await fetch(`${SCRIPT_URL}?clave=${CLAVE_SECRETA}`);
+    const respuesta = await fetch(`${SCRIPT_URL}?key=${CLAVE_SECRETA}`);
     const datos = await respuesta.text();
     
-    res.status(200)
-       .setHeader('Content-Type', 'text/csv')
-       .send(datos);
+    if (datos.includes('ACCESO DENEGADO')) {
+      return res.status(403).send('Error: La clave no coincide');
+    }
+    
+    res.status(200).setHeader('Content-Type', 'text/csv').send(datos);
   } catch (error) {
-    res.status(500).send('Error al obtener datos');
+    res.status(500).send('Error: ' + error.message);
   }
 }
